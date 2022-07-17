44a64029ff8f3a60aaee4b09d36df0f4195aec94
MahdiKoubaa <Mahdi.koubaa@outlook.com>
2022-07-17 18:21:53 +0100

    plz plz work

---
+++ testproject/client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './styles.css';
+import LoginForm from "./components/LoginForm";
+import Login from "./Pages/login"
+import Skeleton from "./Pages/Skeleton";
+import Registre from "./Pages/Registre";
+import ObjectDetection from "./Pages/ObjectDetection"
+import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
+function App(){
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/Skeleton" element={<Skeleton/>}/>
+        <Route path="/Registre" element={<Registre/>}/>
+        <Route path="/Object" element={<ObjectDetection/>}/>
+
+      </Routes>
+    </Router>
+  )
+
+}
+export default App
